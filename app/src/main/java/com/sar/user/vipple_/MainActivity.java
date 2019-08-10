eb99777dcb261e65d5e84540e1e65a5638220d95
abhigupta19 <17ume005@lnmiit.ac.in>
2019-08-10 12:05:49 +0530

    Initial commit

---
+++ app/src/main/java/com/sar/user/vipple_/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sar.user.vipple_;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+
+    }
+}
