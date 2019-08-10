d507d3aa6ae04142b5b398601b4996aa978c23ac
abhigupta19 <17ume005@lnmiit.ac.in>
2019-08-10 14:33:27 +0530

    second

--- app/src/main/java/com/sar/user/vipple_/MainActivity.java
+++ app/src/main/java/com/sar/user/vipple_/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sar.user.vipple_;
 
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Toolbar;
 
@@ -14,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
+        BottomAppBar bottomAppBar=findViewById(R.id.bottomAppBar);
+        setSupportActionBar(bottomAppBar);
+
 
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.title, menu);
+        return true;
+    }
+
 }
