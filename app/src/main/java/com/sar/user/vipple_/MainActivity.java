3f15b83018643ccb78eb9bbdab6fc2338b79feb8
abhigupta19 <17ume005@lnmiit.ac.in>
2019-08-11 22:39:33 +0530

    third

--- app/src/main/java/com/sar/user/vipple_/MainActivity.java
+++ app/src/main/java/com/sar/user/vipple_/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sar.user.vipple_;
 
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,22 +12,27 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Toolbar;
 
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-        BottomAppBar bottomAppBar=findViewById(R.id.bottomAppBar);
-        setSupportActionBar(bottomAppBar);
-
+        SpaceNavigationView spaceNavigationView=findViewById(R.id.space);
+       // spaceNavigationView.setActiveCentreButtonIconColor(getColor(Color.parseColor("#ffffff")));
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
+        spaceNavigationView.addSpaceItem(new SpaceItem("",R.drawable.menu));
+        spaceNavigationView.addSpaceItem(new SpaceItem("",R.drawable.message));
+        spaceNavigationView.addSpaceItem(new SpaceItem("",R.drawable.search));
+        spaceNavigationView.addSpaceItem(new SpaceItem("",R.drawable.home));
 
     }
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.title, menu);
-        return true;
-    }
+
 
 }
